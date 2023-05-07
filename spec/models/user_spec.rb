5f4b6137b3d27e661cf3af3fc1ee6f0eaa06dd92
Bianca Vieira <bncvr@outlook.com>
2023-05-07 17:19:02 -0300

    improve user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,12 +8,26 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to have_secure_password }
+    it { is_expected.to validate_length_of(:password).is_at_least(10) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   end
 
   describe 'password encryption' do
     it 'encrypts the password' do
-      expect(user.password_digest).not_to eq 'password'
+      expect(user.password_digest).not_to eq 'password123'
+    end
+  end
+
+  describe 'email format validation' do
+    let(:user_with_invalid_email) { FactoryBot.build(:user, email: 'invalid-email') }
+
+    it 'allows valid email addresses' do
+      expect(user).to be_valid
+    end
+
+    it 'does not allow invalid email addresses' do
+      expect(user_with_invalid_email).not_to be_valid
     end
   end
 end
