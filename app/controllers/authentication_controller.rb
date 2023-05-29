155ecf3cc29bd6201625dfbfd2d7d3affd294c99
Bianca Vieira <bncvr@outlook.com>
2023-05-28 22:12:07 -0300

    add name to login response

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,7 +7,9 @@ class AuthenticationController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
-      render json: { token:, exp: JsonWebToken::EXPIRES_IN.from_now.strftime('%m-%d-%Y %H:%M'),
+      render json: { token:,
+                     exp: JsonWebToken::EXPIRES_IN.from_now.strftime('%m-%d-%Y %H:%M'),
+                     name: @user.name,
                      email: @user.email }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
