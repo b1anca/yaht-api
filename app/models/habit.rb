5cdf0e4c9f4a902dc164746a6416d39bd6b83aad
Bianca Vieira <bncvr@outlook.com>
2024-04-02 18:04:06 -0300

    add color to habits

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,7 +2,13 @@
 
 class Habit < ApplicationRecord
   validates :name, presence: true
+  validates :color, format: { with: /\A#(?:[0-9a-fA-F]{3}){1,2}\z/,
+                              message: 'must be a valid hex color' },
+                    allow_blank: true
 
   belongs_to :user
+
   has_many :tasks, dependent: :destroy
+
+  default_scope { order(:id) }
 end
